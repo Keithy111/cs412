1d8e5755e3bec0b7c19c597b1b4a338ff63bc291
Keith <keithy@bu.edu>
2024-09-23 14:50:53 -0400

    redirecting home page

--- quotes/views.py
+++ quotes/views.py
@@ -57,3 +57,6 @@ def about(request):
     # Render the 'about.html' template
     return render(request, 'quotes/about.html', context)
 
+def redirect_to_quote(request):
+    return redirect('/quotes/quote/')
+
