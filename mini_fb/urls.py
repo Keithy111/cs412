5332759f79e26d59ec909e79614fb6dc50e10365
Keith <keithy@bu.edu>
2024-10-07 17:05:07 -0400

    Added views and template for mini_fb

--- mini_fb/urls.py
+++ mini_fb/urls.py
@@ -1,9 +1,8 @@
 ## mini_fb/urls.py
 
 from django.urls import path
-# from .views import ShowAllView # our view class definition 
+from .views import ShowAllProfiles  # Import your view class
 
 urlpatterns = [
-    # map the URL (empty string) to the view
-    # path('', ShowAllView.as_view(), name='show_all'), # generic class-based view
+    path('', ShowAllProfiles.as_view(), name='show_all_profiles'),  # Map '' to ShowAllProfilesView
 ]
--- mini_fb/views.py
+++ mini_fb/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+#mini_fb/views.py
+
+from .models import Profile # import the models(Profile)
+from django.views.generic import ListView
 
 # Create your views here.
+class ShowAllProfiles(ListView):
+    '''Create a subclass of ListView to display all blog articles.'''
+
+    model = Profile # retrieve objects of type Profile from the database
+    template_name = 'mini_fb/show_all_profiles.html'
+    context_object_name = 'profiles' # how to find the data in the template file
