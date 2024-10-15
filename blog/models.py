268a74e07df8917b808faa60077dc9555a5e0416
Keith <keithy@bu.edu>
2024-10-15 02:05:32 -0400

    example from class

--- blog/models.py
+++ blog/models.py
@@ -19,5 +19,24 @@ class Article(models.Model):
     '''Return a string representation of this object.'''
 
     return f'{self.title} by {self.author}'
+  
+  def get_comments(self):
+        '''Return all of the comments about this article.'''
+        comments = Comment.objects.filter(article=self)
+        return comments
 
+class Comment(models.Model):
+    '''Encapsulate the idea of a Comment on an Article.'''
+    
+    # data attributes of a Comment:
+
+    # model the 1 to many relationship with Article (foreign key)
+    article = models.ForeignKey("Article", on_delete=models.CASCADE) ## if article gets deleted delete the comments as well
+    author = models.TextField(blank=False)
+    text = models.TextField(blank=False)
+    published = models.DateTimeField(auto_now=True)
+    
+    def __str__(self):
+        '''Return a string representation of this Comment object.'''
+        return f'{self.text}'
 
